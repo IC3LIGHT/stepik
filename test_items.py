bc348b895cf5cc1c2086b61626dcc07184d891c6
IC3LIGHT <112938558+IC3LIGHT@users.noreply.github.com>
2023-05-20 14:24:52 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -7,5 +7,4 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_search_button_shopping_cart(browser):
     browser.get(link)
     time.sleep(3)
-    button = browser.find_element(By.CLASS_NAME, "btn.btn-lg.btn-primary.btn-add-to-basket")
-    assert button, "Should be button with the addition of the product to cart"
+    assert browser.find_element(By.CLASS_NAME, "btn.btn-lg.btn-primary.btn-add-to-basket")

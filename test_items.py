d7c5d6f1cfaf4c8df6aaec11ea29479a8335801d
IC3LIGHT <112938558+IC3LIGHT@users.noreply.github.com>
2023-05-20 14:32:03 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -7,4 +7,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_search_button_shopping_cart(browser):
     browser.get(link)
     time.sleep(3)
-    assert browser.find_element(By.CLASS_NAME, "btn.btn-lg.btn-primary.btn-add-to-basket")
+    button = browser.find_element(By.CSS_SELECTOR, ".add-to-basket .btn")
+    assert button, "The button not found"
